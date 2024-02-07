2ade166bda3b242506b6d8cf5593e2b1da5f09be
AndrewPoore <andrewpoore27@gmail.com>
2024-02-06 17:06:07 -0700

    Week 5 Team activity initial

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 import productList from "./productList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
+loadHeaderFooter();
 productList(".product-list", "tents");
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -25,7 +25,7 @@ export function setClick(selector, callback) {
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param); // ? is this supposed to be Param
+  const product = urlParams.get("product");
   return product; //! Maybe change this parameter if needed
 }
 
@@ -42,4 +42,47 @@ export function renderListWithTemplate(
   const htmlString = list.map(templateFn);
 
   parentElement.insertAdjacentHTML(position, htmlString.join(""));
+}
+
+export async function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data,
+  callback,
+  position = "afterbegin",
+  clear = true
+) {
+  // get template using function...no need to loop this time.
+  if (clear) {
+      parentElement.innerHTML = "";
+  }
+  const htmlString = await templateFn(data);
+  parentElement.insertAdjacentHTML(position, htmlString);
+  if(callback) {
+      callback(data);
+  }
+}
+
+function loadTemplate(path) {
+  // wait what?  we are returning a new function? 
+  // this is called currying and can be very helpful.
+  return async function () {
+      const res = await fetch(path);
+      if (res.ok) {
+      const html = await res.text();
+      return html;
+      }
+  };
+} 
+
+
+export async function loadHeaderFooter() {
+
+  const headerTemplateFn = loadTemplate("/partials/header.html");
+  const footerTemplateFn = loadTemplate("/partials/footer.html");
+
+  const headerEl = document.querySelector('#main-header');
+  const footerEl = document.querySelector('#main-footer');
+  renderWithTemplate(headerTemplateFn, headerEl);
+  renderWithTemplate(footerTemplateFn, footerEl);
 }
