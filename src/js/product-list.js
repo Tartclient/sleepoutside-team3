6b9ae773803159003140fc5773f1396d19959b4c
Tartclient <henry.ariasguerrero@gmail.com>
2024-02-13 16:13:56 -0800

    test team5

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,8 +1,10 @@
-// import productList from "./productList.mjs";
-// productList(".product-list", "tents");
+// import productList from "./productList.mjs"; //!delete?
+// productList(".product-list", "tents"); //!delete?
 import productList from "./productList.mjs";
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 const category = getParam("category");
+console.log("category");
+console.log(category);
 productList(".product-list", category);
