7f5481fba82f6d0fc47629e1b4007823ded486db
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2024-02-17 13:01:21 -0700

    Fix shopping cart error when null

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import { updateCartCounter } from "./header.js";
+
 loadHeaderFooter();
 updateCartCounter();
+
+if (!localStorage.getItem("visited")) {
+    // myFunction();
+    localStorage.setItem("visited", true);
+}
