3718f4736d057762714c8ac8c3b95bece92a4da9
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2024-02-21 16:10:52 -0700

    Modal is added when first time launch of website

--- src/js/main.js
+++ src/js/main.js
@@ -5,24 +5,48 @@ loadHeaderFooter();
 updateCartCounter();
 
 if (!localStorage.getItem("visited")) {
-    // myFunction();
+    createGiveAwayModal()
     localStorage.setItem("visited", true);
 }
 
+function createGiveAwayModal() {
+    const modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+    modalContent.innerHTML += "" +
+        "      <div class=\"modal-header\">" +
+        "        <span class=\"close\">&times;</span>" +
+        "        <h2>Gear Up for Adventure: Win Big in Our Ultimate Camping Giveaway!</h2>" +
+        "      </div>" +
+        "      <div class=\"modal-body\">" +
+        "        <p>Register to be entered into the giveaway!</p>" +
+        "        <p>One lucky registrar will be entered to win a tent of their choice!</p>" +
+        "        <form id=\"giveaway\" name=\"register-form\" accept-charset=\"utf-8\" action=\"\"" +
+        "          method=\"\">" +
+        "          <fieldset id=\"giveaway-inputs\">" +
+        "            <label for=\"full-name\">Full Name</label>" +
+        "            <input type=\"text\" name=\"name\" id=\"full-name\" placeholder=\"First and Last\" required>" +
+        "            <label for=\"email-address\">Email Address</label>" +
+        "            <input type=\"email\" name=\"email\" id=\"email-address\" placeholder=\"email@site.com\" required>" +
+        "            <input type=\"hidden\" name=\"subject\" id=\"email-subject\" value=\"Give Away Registration\">" +
+        "          </fieldset>" +
+        "          <input type=\"submit\" value=\"Register\">" +
+        "        </form>" +
+        "      </div>" +
+        "      <div class=\"modal-footer\">" +
+        "        <h3>Terms May Apply: Gear Galore Grab - Winners must navigate a simulated wilderness obstacle course to claim their prize.</h3>" +
+        "      </div>" +
+        "";
+    const main = document.querySelector("main");
+    main.append(modalContent);
+}
+
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.querySelector(".modal-content");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelectorAll(".close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 };
